544a8a9126acb4f7a17a359d8436dfd4ac7027b1
Laura López <lauralopez166@gmail.com>
2023-10-17 10:55:29 +0200

    creadas las funciones

---
+++ script.js
@@ -0,0 +1,21 @@
+const inventario =[
+{ id: 1, nombre: "Camiseta", cantidad: 50, precio: 15},
+{ id: 2, nombre: "Pantalones", cantidad: 30, precio: 30},
+{ id: 3, nombre: "Zapatos", cantidad: 20, precio: 50},
+];
+
+for (let i = 0; i < inventario.length; i++) {
+    const element = inventario[i];
+}
+
+//En caso de que no lo encuentre, que salga no encontrado
+const buscarProducto = () =>{};
+
+//Agregar inventario o si no actualizarlo
+const actualizarInventario = () =>{};
+
+//Eliminar el producto
+const eliminarProducto = () =>{};
+
+//Mostrar inventario final
+const mostrarInventario = ()=>{};
