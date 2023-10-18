2af2915270d7b057a74c84ea6b4086abc40882de
Laura López <99351746+laurity@users.noreply.github.com>
2023-10-18 09:44:37 +0000

    seguir en casa

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+const producto = document.getElementById('product');
+
+
 const inventario =[
 { id: 1, nombre: "Camiseta", cantidad: 50, precio: 15},
 { id: 2, nombre: "Pantalones", cantidad: 30, precio: 30},
 { id: 3, nombre: "Zapatos", cantidad: 20, precio: 50},
 ];
 
-for (let i = 0; i < inventario.length; i++) {
-    const element = inventario[i];
-}
+const totalInventario='';
 
 //En caso de que no lo encuentre, que salga no encontrado
-const buscarProducto = () =>{};
+const buscarProducto = () =>{
+};
 
 //Agregar inventario o si no actualizarlo
-const actualizarInventario = () =>{};
+const actualizarInventario = () =>{
+    for (let i = 5; i < 25; i++) {
+        inventario = ['id: i, nombre: "${text}", cantidad: "${number}", precio: "${number}'];
+};
+}
+
 
 //Eliminar el producto
 const eliminarProducto = () =>{};
