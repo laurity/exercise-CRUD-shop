6dfb4340cf7cca23c62a7235c644764f529b6c72
Laura López <lauralopez166@gmail.com>
2023-10-22 17:48:37 +0200

    Empezamos de nuevo

--- scripts/main.js
+++ scripts/main.js
@@ -4,60 +4,40 @@ const newProduct = document.getElementById('new');
 const updateProduct = document.getElementById('update');
 const deleteProduct = document.getElementById('delete');
 const actionButtons = document.getElementById('action-inputs');
-const addProduct = document.getElementById('add-rows');
+const tbody = document.getElementById("add-rows");
 
-document.addEventListener('DOMContentLoaded', () =>{
-    const inventario =[
-        { id: 1, nombre: "Camisetas", cantidad: 50, precio: 15},
-        { id: 2, nombre: "Pantalones", cantidad: 30, precio: 30},
-        { id: 3, nombre: "Zapatos", cantidad: 20, precio: 50},
-        ];
-        
-        /**
-         * PARTE 1
-         */
-        
-        //Añadimos nueva sección
-        inventario.push({ id: 4, nombre: "Gorras", cantidad: 40, precio: 18 });
-        
-        //Actualizar Camisetas
-        const tShirts = inventario.find(inventario => inventario.nombre === "Camisetas");
-        if (tShirts){
-            tShirts.cantidad = 40;
-            tShirts.precio = 18;
-        }
-        console.log(inventario);
-        console.log(tShirts);    
+document.addEventListener('DOMContentLoaded', (actualizarInventario) => {
+    const inventario = [
+        { id: 1, nombre: "Camisetas", cantidad: 50, precio: 15 },
+        { id: 2, nombre: "Pantalones", cantidad: 30, precio: 30 },
+        { id: 3, nombre: "Zapatos", cantidad: 20, precio: 50 },
+    ];
 
-   //Variable para sumar inventario
-let totalInventario = 0;     
+    // PARTE 1
 
-//Mostrar suma inventario final
+    // Añadimos nueva sección
+    inventario.push({ id: 4, nombre: "Gorras", cantidad: 40, precio: 18 });
+
+    // Actualizar Camisetas
+    const tShirts = inventario.find((item) => item.nombre === "Camisetas");
+    if (tShirts) {
+        tShirts.cantidad = 40;
+        tShirts.precio = 18;
+    }
+
+    console.log(inventario);
+    console.log(tShirts);
+
+    // Variable para sumar inventario
+    let totalInventario = 0;
+
+    // Mostrar suma del inventario final
     for (const item of inventario) {
         totalInventario += item.cantidad * item.precio;
     }
-    console.log("Es " + totalInventario);
+    console.log("El valor total del inventario es " + totalInventario);
 
-/**
- * PARTE 2  
- */
+    //------------------------------------------------------------------------------
 
-//En caso de que no lo encuentre, que salga no encontrado
-const buscarProducto = (product) =>{
-    for (const item of inventario) {
-        if (item.nombre === product){
-            return item;
-        }
-    return "no se encuentra en el inventario";
-}
-};
-
-//Agregar inventario o si no actualizarlo
-const actualizarInventario = (id, nombre, cantidad, precio) => {
-
-};
-
-//Eliminar el producto
-const eliminarProducto = () =>{};
-
+    
 });
